8594fbc6a9149b11e12aa8fd7e4d1f396b6291f8
emanuel caro <darkemlord@gmail.com>
2024-06-18 23:25:12 +0900

    refs: main variables done

--- main.py
+++ main.py
@@ -42,8 +42,64 @@ WHITE = (255, 255, 255)
 
 font = pg.font.Font("./assets/WashYourHand.ttf", 32)
 
-running = True
+# Set text
+
+points_text = font.render("Burger Points: " + str(burger_points), True, ORANGE)
+points_rect = points_text.get_rect()
+points_rect.topleft = (10, 10)
+
+score_text = font.render("Score: " + str(score), True, ORANGE)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 50)
+
+title_text = font.render("Burger Dog", True, ORANGE)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOW_WIDTH // 2
+title_rect.y = 10
+
+eaten_text = font.render("Burgers Eaten: " + str(burgers_eaten), True, ORANGE)
+eaten_rect = eaten_text.get_rect()
+eaten_rect.centerx = WINDOW_WIDTH // 2
+eaten_rect.y = 50
+
+lives_text = font.render("Lives: " + str(player_lives), True, ORANGE)
+lives_rect = lives_text.get_rect()
+lives_rect.topright = (WINDOW_WIDTH - 10, 10)
+
+boost_text = font.render("Boost: " + str(boost_level), True, ORANGE)
+boost_rect = boost_text.get_rect()
+boost_rect.topright = (WINDOW_WIDTH - 10, 50)
+
+game_over_text = font.render("Game Over", True, ORANGE)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+
+continue_text = font.render("Press any key to continue", True, ORANGE)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 64)
+
+# Set game sounds and music
+
+bark_sound = pg.mixer.Sound("./assets/bark_sound.wav")
+miss_sound = pg.mixer.Sound("./assets/miss_sound.wav")
+
+# Set images
+
+player_image_right = pg.image.load("./assets/dog_right.png")
+player_image_left = pg.image.load("./assets/dog_left.png")
+
+player_image = player_image_left
+player_rect = player_image.get_rect()
+player_rect.centerx = WINDOW_WIDTH // 2
+player_rect.bottom = WINDOW_HEIGHT
+
+burger_image = pg.image.load("./assets/burger.png")
+burger_rect = burger_image.get_rect()
+burger_rect.topleft = (random.randint(0, WINDOW_WIDTH - 32), -BUFFER_DISTANCE)
+
 # The main game loop
+pg.mixer.music.play()
+running = True
 while running:
     # Check if the user quits the game
     for event in pg.event.get():
